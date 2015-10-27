6a8c7a004c2d2499cd6601efaa5f81e9b2c944e1
HoffsMH <hecker.matthew@gmail.com>
2015-10-26 20:29:22 -0600

    added column to users table
    users table now has a string for profile image url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027011819) do
+ActiveRecord::Schema.define(version: 20151027022745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151027011819) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "profile_image_url"
   end
 
 end
