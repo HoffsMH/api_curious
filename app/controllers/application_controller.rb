cce09d463c57b73a73fdfad36cce554106c877de
HoffsMH <hecker.matthew@gmail.com>
2015-10-27 15:26:54 -0600

    changed application controller
    applications controller helper methods
    current user no longer returns nil when a user is not logged in but the user class itself

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    user_id = session[:user_id]
+    @current_user = User.find_by(id: user_id) || User
   end
 
   def current_user?
-    !!current_user
+    current_user.is_a? User
+  end
+
+  def twitter_client(access_token=nil, access_token_secret=nil)
+    @twitter_client                   ||= TwitterClient.new
+    @twitter_client.access_token        = access_token || current_user.oauth_token
+    @twitter_client.access_token_secret = access_token_secret || current_user.oauth_token_secret
+    @twitter_client
   end
 end
