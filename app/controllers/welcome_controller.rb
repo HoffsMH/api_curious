5676fadd37db200ba619af93fc0e36112f138505
HoffsMH <hecker.matthew@gmail.com>
2015-10-26 20:18:08 -0600

    created current user helpers
    to applications controller
    we now have
    current_user
    current_user?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    render landing_page[current_user?]
+  end
 
+  private
+  def landing_page
+    {
+      false =>  '/welcome/index_not_logged_in',
+      true =>  '/welcome/index',
+    }
   end
 end
