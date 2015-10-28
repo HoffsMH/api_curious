cdbdcf526c246cc1a7e61cf73b2b844418cf02cd
HoffsMH <hecker.matthew@gmail.com>
2015-10-27 19:40:28 -0600

    started spec for application controller
    we need to finish testing for the helper methods inside of app controller
    
    current_user and current_client

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
+  context "when calling the current_user method" do
+     it 'if there is no user, retuns the class User' do
+       expect(@controller.current_user).to eq(User)
+     end
 
+     it 'if there is a user, returns the current user' do
+       user = create(:user)
+       session[:user_id] = 1
+       expect(@controller.current_user).to eq(User)
+     end
+  end
 end
+
+
+
+
