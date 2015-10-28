c7254cb7b3396db421fcda2d23f9ae8159e1d2a7
HoffsMH <hecker.matthew@gmail.com>
2015-10-27 20:46:43 -0600

    moved twitter_client to user presenter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,4 @@ class ApplicationController < ActionController::Base
   def current_user?
     current_user.is_a? User
   end
-
-  def twitter_client(access_token=nil, access_token_secret=nil)
-    @twitter_client                   ||= TwitterClient.new
-    @twitter_client.access_token        = access_token        || current_user.oauth_token
-    @twitter_client.access_token_secret = access_token_secret || current_user.oauth_token_secret
-    @twitter_client
-  end
 end
--- app/models/user_presenter.rb
+++ app/models/user_presenter.rb
@@ -1,11 +1,19 @@
 class UserPresenter
   attr_accessor :user
   def initialize(user=nil)
+    binding.pry
     @user = user
   end
 
   def profile_picture
     @user.profile_image_url
+  end
+
+  def twitter_client(access_token=nil, access_token_secret=nil)
+    @twitter_client                   ||= TwitterClient.new
+    @twitter_client.access_token        = access_token        || @user.oauth_token
+    @twitter_client.access_token_secret = access_token_secret || @user.oauth_token_secret
+    @twitter_client
   end
 end
 
