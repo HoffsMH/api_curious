17b7c27fa00ff9a56ad196341c34e35c2921557e
HoffsMH <hecker.matthew@gmail.com>
2015-10-27 21:05:02 -0600

    added a feed function
    user presenter can now get a users home feed

--- app/models/user_presenter.rb
+++ app/models/user_presenter.rb
@@ -8,11 +8,15 @@ class UserPresenter
     @user.profile_image_url
   end
 
+  def feed
+    twitter_client.home_timeline
+  end
+
   def twitter_client(access_token=nil, access_token_secret=nil)
-    @twitter_client                   ||= TwitterClient.new
-    @twitter_client.access_token        = access_token        || @user.oauth_token
-    @twitter_client.access_token_secret = access_token_secret || @user.oauth_token_secret
-    @twitter_client
+    twitter_client                   ||= TwitterClient.new
+    twitter_client.access_token        = access_token        || @user.oauth_token
+    twitter_client.access_token_secret = access_token_secret || @user.oauth_token_secret
+    twitter_client
   end
 end
 
