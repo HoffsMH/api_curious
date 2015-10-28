fa775ba775df74cd3fa99e752b32bd8f5c031379
HoffsMH <hecker.matthew@gmail.com>
2015-10-28 12:33:30 -0600

    new test
    we are now testing for log out functionality

--- spec/features/user_logs_in_with_twitter_spec.rb
+++ spec/features/user_logs_in_with_twitter_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
 
-RSpec.feature "user loggs in with twitter" do
+RSpec.feature "user loggs in with twitter", vcr: true do
   context "when not logged in" do
 
-    scenario "can login" ,vcr: true do
+    scenario "can login" do
       stub_omniauth
       visit '/'
       expect(page.status_code).to eq(200)
 
       click_link "Login with Twitter"
       expect(page).to have_content("worace")
+    end
+  end
+
+  context "when  logged in" do
+    before(:each) do
+      stub_omniauth
+      visit '/'
+      click_link "Login with Twitter"
+    end
+
+    scenario "can login"  do
+      click_link "Log out"
+      expect(page).to have_content("Login with Twitter")
     end
   end
 end
