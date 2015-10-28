469d71b338db32c777a2244281501c3b9b245598
HoffsMH <hecker.matthew@gmail.com>
2015-10-27 20:02:30 -0600

    basic testing for twitter gem
    we now have coverage for the scaffolding we will be making our api request from

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -8,8 +8,22 @@ RSpec.describe ApplicationController, type: :controller do
 
      it 'if there is a user, returns the current user' do
        user = create(:user)
-       session[:user_id] = 1
-       expect(@controller.current_user).to eq(User)
+       session[:user_id] = user.id
+       expect(@controller.current_user).to eq(user)
+     end
+  end
+
+  context "when calling the twitter_client method" do
+     it 'if there is no user, returns client with no user data' do
+       expect(@controller.twitter_client.access_token).to be_nil
+       expect(@controller.twitter_client.access_token_secret).to be_nil
+     end
+
+     it 'if there is a user, returns client with the current users data' do
+       user = create(:user)
+       session[:user_id] = user.id
+       expect(@controller.twitter_client.access_token).to eq(user.oauth_token)
+       expect(@controller.twitter_client.access_token_secret).to eq(user.oauth_token_secret)
      end
   end
 end
