ad48b1e3abe4835ebfa7a977de4421ad334875f9
HoffsMH <hecker.matthew@gmail.com>
2015-10-29 13:32:18 -0600

    !

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    render plain: request.env["omniauth.auth"]
+    render plain: request.env["omniauth.auth"].info.location
     # user = User.from_omniauth(request.env["omniauth.auth"])
     # load_session(user)
     # redirect_to root_path
