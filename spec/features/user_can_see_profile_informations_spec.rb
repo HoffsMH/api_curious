47d7b2398f7c4dfddee5ff892b611b3642bbd7da
HoffsMH <hecker.matthew@gmail.com>
2015-10-28 20:01:21 -0600

    filled out feature test
    filled out preliminary test for profile information display

--- spec/features/user_can_see_profile_informations_spec.rb
+++ spec/features/user_can_see_profile_informations_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature "UserCanSeeProfileInformations", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "UserCanSeeProfileInformations", type: :feature, vcr: true do
+  context "when logged in" do
+    it "shows our profile information" do
+      stub_omniauth
+      visit "/"
+
+      expect(page.status_code).to eq(200)
+      click_link "Login with Twitter"
+      expect(page).to have_content("Tweets: 22 ")
+      expect(page).to have_content("Following: 22 ")
+      expect(page).to have_content("Tweets: 22 ")
+    end
+  end
 end
