bba11c9525275d9e8330564aa822bede4d821460
HoffsMH <hecker.matthew@gmail.com>
2015-10-28 23:55:45 -0600

    added tests
    we are not testing that a user can successfully favorite and unfavorite a tweet

--- spec/features/user_can_favorite_a_tweets_spec.rb
+++ spec/features/user_can_favorite_a_tweets_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.feature "UserCanFavoriteATweets", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "UserCanFavoriteATweets", type: :feature, vcr: true do
+  context "when logged in" do
+    before(:each) do
+      stub_omniauth
+      visit "/"
+      click_link "Login with Twitter"
+    end
+
+    it "lets us favorite a tweet" do
+      first('form').click_link_or_button('favorite')
+
+      expect(page).to have_button("unfavorite")
+    end
+
+    it "lets us unfavorite a tweet" do
+      click_link_or_button('unfavorite')
+
+      expect(page).not_to have_button("unfavorite")
+    end
+  end
 end
